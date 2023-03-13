3ef7ade1349d2692fb62eb65356716f67cd0741d
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2023-03-13 14:36:46 +0100

    added markup for the mobile navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,11 @@ import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
     const [active, setActive] = useState("");
+    const [toggle, setToggle] = useState(false);
 
     return (
         <nav
-            className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+            className={`${styles.paddingX} w-full flex items-center py-4 fixed top-0 z-20 bg-primary`}
         >
             <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
                 <Link
@@ -26,8 +27,8 @@ const Navbar = () => {
                         alt="logo"
                         className="w-16 h-16 object-contain"
                     />
-                    <p className="text-white text-[18px] cursor-pointer">
-                        Dusko{" "}
+                    <p className="text-white text-[18px] cursor-pointer font-bold flex">
+                        Dusko &nbsp;
                         <span className="sm:block hidden">| Portfolio</span>
                     </p>
                 </Link>
@@ -48,8 +49,41 @@ const Navbar = () => {
                     ))}
                 </ul>
 
-                {/* Mobile navigation */}
-                <div></div>
+                {/* Mobile navigation - icons */}
+                <div className="sm:hidden flex flex-1 justify-end items-center">
+                    <img
+                        src={toggle ? close : menu}
+                        alt="menu"
+                        className="w-[28px] h-[28px] object-contain cursor-pointer"
+                        onClick={() => setToggle(!toggle)}
+                    />
+
+                    {/* Mobile navigation - links */}
+                    <div
+                        className={`${
+                            !toggle ? "hidden" : "flex"
+                        } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+                    >
+                        <ul className="list-none flex justify-end items-start flex-col gap-4">
+                            {navLinks.map((link) => (
+                                <li
+                                    key={link.id}
+                                    className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                                        active === link.title
+                                            ? "text-white"
+                                            : "text-secondary"
+                                    }`}
+                                    onClick={() => {
+                                        setToggle(!toggle);
+                                        setActive(link.title);
+                                    }}
+                                >
+                                    <a href={`#${link.id}`}>{link.title}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
         </nav>
     );
