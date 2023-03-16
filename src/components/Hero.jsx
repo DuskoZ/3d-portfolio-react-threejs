3ad7ae8aa9d1d064d5ceab61229ff986d625a1fc
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2023-03-16 22:07:16 +0100

    updated component markup

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
                         Hi, I'm <span className="text-[#915EFF]">John</span>
                     </h1>
                     <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-                        I develop 3D visuals, user{" "}
+                        I develop 3D visuals, user
                         <br className="sm:block hidden" />
                         interfaces and web applications
                     </p>
@@ -27,6 +27,25 @@ const Hero = () => {
             </div>
 
             <ComputersCanvas />
+
+            {/* Icon - Jump to the next section */}
+            <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+                <a href="#about">
+                    <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+                        <motion.div
+                            animate={{
+                                y: [0, 24, 0],
+                            }}
+                            transition={{
+                                duration: 1.5,
+                                repeat: Infinity,
+                                repeatType: "loop",
+                            }}
+                            className="w-3 h-3 rounded-full bg-secondary mb-1"
+                        />
+                    </div>
+                </a>
+            </div>
         </section>
     );
 };
