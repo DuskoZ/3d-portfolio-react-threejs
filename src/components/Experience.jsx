9c77e4feb05082e5d5d6cc168d42b4fea5274ff9
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2023-03-19 19:51:39 +0100

    started creating component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import {
+    VerticalTimeline,
+    VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
+
+const ExperienceCard = ({ experience }) => {
+    return <div>Card</div>;
+};
 
 const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
-}
+    return (
+        <>
+            <motion.div variants={textVariant()}>
+                <p className={`${styles.sectionSubText} text-center`}>
+                    What I have done so far
+                </p>
+                <h2 className={`${styles.sectionHeadText} text-center`}>
+                    Work Experience.
+                </h2>
+            </motion.div>
 
-export default Experience
+            <div className="mt-20 flex flex-col">
+                <VerticalTimeline>
+                    {experiences.map((experience, index) => (
+                        <ExperienceCard key={index} experience={experience} />
+                    ))}
+                </VerticalTimeline>
+            </div>
+        </>
+    );
+};
+
+export default SectionWrapper(Experience, "work");
