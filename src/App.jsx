72a303e2978c0478e33db6609cf411d3109dcb1e
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2023-03-27 23:11:07 +0200

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,20 +16,21 @@ const App = () => {
     return (
         <BrowserRouter>
             <div className="relative z-0 bg-primary">
-                <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"></div>
-                <Navbar />
-                <Hero />
-            </div>
+                <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+                    <Navbar />
+                    <Hero />
+                </div>
 
-            <About />
-            <Experience />
-            <Tech />
-            <Works />
-            <Feedbacks />
+                <About />
+                <Experience />
+                <Tech />
+                <Works />
+                <Feedbacks />
 
-            <div className="relative z-0">
-                <Contact />
-                <StarsCanvas />
+                <div className="relative z-0">
+                    <Contact />
+                    <StarsCanvas />
+                </div>
             </div>
         </BrowserRouter>
     );
