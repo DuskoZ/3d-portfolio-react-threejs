a67128f375c8790b2156645781c38308795e836c
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2023-03-28 15:38:24 +0200

    updated component

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import { testimonials } from "../constants";
+
+const FeedbackCard = () => {};
 
 const Feedbacks = () => {
-  return (
-    <div>Feedbacks</div>
-  )
-}
+    return (
+        <div className={`mt-12 bg-black-100 rounded-[20px]`}>
+            <div
+                className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
+            >
+                <motion.div variants={textVariant()}>
+                    <p className={styles.sectionSubText}>What others say</p>
+                    <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+                </motion.div>
+            </div>
+            <div
+                className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}
+            >
+                {testimonials.map((testimonial, index) => (
+                    <FeedbackCard
+                        key={testimonial.name}
+                        index={index}
+                        {...testimonial}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
 
-export default Feedbacks
+export default SectionWrapper(Feedbacks, "");
