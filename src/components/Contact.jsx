e45ea9cafefce4bacbc2459552a8cd88df71fa15
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2023-03-30 14:33:34 +0200

    started component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+    const formRef = useRef();
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+    const [loading, setLoading] = useState(false);
 
-export default Contact
+    const handleChange = (e) => {};
+
+    const handleSubmit = (e) => {};
+
+    return (
+        <div className="xl:mt-12 xl:flex-row flex flex-col-reverse gap-10 overflow-hidden">
+            <motion.div
+                variants={slideIn("left", "tween", 0.2, 1)}
+                className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+            ></motion.div>
+        </div>
+    );
+};
+
+export default SectionWrapper(Contact, "contact");
