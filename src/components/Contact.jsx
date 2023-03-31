1d2f9e210056a22b44494b1acae59fc1d1f0c5bb
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2023-03-31 16:00:45 +0200

    updated component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,7 +25,56 @@ const Contact = () => {
             <motion.div
                 variants={slideIn("left", "tween", 0.2, 1)}
                 className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
-            ></motion.div>
+            >
+                <p className={styles.sectionSubText}>Get in touch</p>
+                <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+                <form
+                    ref={formRef}
+                    onSubmit={handleSubmit}
+                    className="mt-12 flex flex-col gap-8"
+                >
+                    <label className="flex flex-col">
+                        <span className="text-white font-medium mb-4">
+                            Your Name
+                        </span>
+                        <input
+                            type="text"
+                            name="name"
+                            value={form.name}
+                            onChange={handleChange}
+                            placeholder="What's your good name?"
+                            className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+                        />
+                    </label>
+                    <label className="flex flex-col">
+                        <span className="text-white font-medium mb-4">
+                            Your email
+                        </span>
+                        <input
+                            type="email"
+                            name="email"
+                            value={form.email}
+                            onChange={handleChange}
+                            placeholder="What's your web address?"
+                            className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+                        />
+                    </label>
+                    <label className="flex flex-col">
+                        <span className="text-white font-medium mb-4">
+                            Your Message
+                        </span>
+                        <textarea
+                            rows={7}
+                            name="message"
+                            value={form.message}
+                            onChange={handleChange}
+                            placeholder="What you want to say?"
+                            className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+                        />
+                    </label>
+                </form>
+            </motion.div>
         </div>
     );
 };
