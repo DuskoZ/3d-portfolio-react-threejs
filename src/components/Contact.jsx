74553fe07a09347089abe4c820051ad0e3a0485b
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2023-04-01 22:02:29 +0200

    updated component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -73,7 +73,21 @@ const Contact = () => {
                             className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
                         />
                     </label>
+
+                    <button
+                        type="submit"
+                        className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+                    >
+                        {loading ? "Sending..." : "Send"}
+                    </button>
                 </form>
+            </motion.div>
+
+            <motion.div
+                variants={slideIn("right", "tween", 0.2, 1)}
+                className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+            >
+                <EarthCanvas />
             </motion.div>
         </div>
     );
