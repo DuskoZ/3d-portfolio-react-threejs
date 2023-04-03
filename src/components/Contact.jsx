af06e8286b36794fd1e447e09cc9a6711becf131
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2023-04-03 18:19:12 +0200

    minor fixes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -56,7 +56,7 @@ const Contact = () => {
                             name="email"
                             value={form.email}
                             onChange={handleChange}
-                            placeholder="What's your web address?"
+                            placeholder="What's your email address?"
                             className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
                         />
                     </label>
@@ -69,7 +69,7 @@ const Contact = () => {
                             name="message"
                             value={form.message}
                             onChange={handleChange}
-                            placeholder="What you want to say?"
+                            placeholder="What do you want to say?"
                             className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
                         />
                     </label>
