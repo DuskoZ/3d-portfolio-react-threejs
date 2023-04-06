3df2fc0433efafbf24d454eabadafd737db747b2
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2023-04-06 17:35:27 +0200

    added handleSubmit method

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,6 +29,40 @@ const Contact = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         setLoading(true);
+
+        emailjs
+            .send(
+                import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+                import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+                {
+                    from_name: form.name,
+                    to_name: "JavaScript Mastery",
+                    from_email: form.email,
+                    to_email: "example@email.com",
+                    message: form.message,
+                },
+                import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+            )
+            .then(
+                () => {
+                    setLoading(false);
+                    alert(
+                        "Thank you. I will get back to you as soon as possible."
+                    );
+
+                    setForm({
+                        name: "",
+                        email: "",
+                        message: "",
+                    });
+                },
+                (error) => {
+                    setLoading(false);
+                    console.error(error);
+
+                    alert("Something went wrong. Please try again.");
+                }
+            );
     };
 
     return (
